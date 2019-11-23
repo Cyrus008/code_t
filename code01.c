ecf2c848f9b906894332c5b4e97f95bb9d61435e
pramod Maurya <pramod.nexgen@gmail.com>
2019-11-24 01:33:31 +0530

    This is final working code....git add code01.c

--- code01.c
+++ code01.c
@@ -1,40 +1,53 @@
+/* About Code :
+ *              This code is written for ratating desired element from left to write
+ * Author     : Pramod Kumar Maurya
+ * Date       : 23/11/2019            
+ **/
+
 #include<stdio.h>
 #include<stdlib.h>
 
-int* temp_buffering(int *array,int d,int *temp)
+int* Temp_buffering(int *array,int shift_element,int *temp)
 {
-   int i=0;
-   while (i<d)
-   {
-       temp[i]=array[i];
-       i++;
-       }
-   return temp;
+    int i=0;
+             while (i<shift_element)
+            {
+                   temp[i]=array[i];
+                   i++;
+             }
+    return temp;
 }
 
-int* A_rot_fun(int *array,int len)
+int *Array_swapping(int *array,int len,int shift_element)
 {
-  int d=2,temp[2];
-  temp_buffering(array,d,temp);
-  for(int i=0;i<d;i++)
-    {
-      for(int j=0;j<len;j++)
-      array[j]=array[j+1];
-    }
-                 array[4] = temp[0]; 
-                 array[5] = temp[1];
-  return array;
+   for(int i=0;i<shift_element;i++)
+          {
+            for(int j=0;j<len;j++)
+                      array[j]=array[j+1];
+          }
+    return array;
 }
 
-
+int* Array_Roation(int *array,int len,int shift_element)
+{
+      int temp[shift_element];
+      Temp_buffering(array,shift_element,temp);  
+      Array_swapping(array,len,shift_element); 
+      for(int i=0;i<shift_element;i++)
+         {
+            for(int j=shift_element-i;j>0;j--)
+                      array[len-j] = temp[i];        
+         }   
+       return array;
+}
 
 int main()
 {
-   int X[] ={1,4,6,7,9,8};
+   int X[] ={1,4,6,7,9,8,5},d=6;
    int len = sizeof(X)/sizeof(X[0]);
-   A_rot_fun(X,len); 
-   for(int x=0;x<len;x++) 
-            printf("%d ",*(X+x));
-            printf("\n");  
+   Array_Roation(X,len,d); 
+      for(int x=0;x<len;x++) 
+              printf("%d ",*(X+x));
+              printf("\n");  
    return EXIT_SUCCESS ;
 }
