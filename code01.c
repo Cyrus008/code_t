5389a78638608b18f33663cccf8ade48bf64b833
pramod Maurya <pramod.nexgen@gmail.com>
2019-11-23 19:41:29 +0530

    This is Temporary Fixed

--- code01.c
+++ code01.c
@@ -1,9 +1,41 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+int* temp_buffering(int *array,int d,int *temp)
+{
+   int i=0;
+   while (i<d)
+   {
+       temp[i]=array[i];
+       i++;
+       }
+   return temp;
+}
+
+int* A_rot_fun(int *array,int len)
+{
+  int d=2,temp[2];
+  temp_buffering(array,d,temp);
+  for(int i=0;i<d;i++)
+    {
+      for(int j=0;j<len;j++)
+      array[j]=array[j+1];
+    }
+         
+                 array[4] = temp[0];  //This is temporary fixed
+                 array[5] = temp[1];
+  return array;
+}
+
+
+
 int main()
 {
-   int X[] ={1,4,6,7,9,0};
-   int len = sizeof(X)/sizeof(X[0]); 
-   printf("S");  
+   int X[] ={1,4,6,7,9,8};
+   int len = sizeof(X)/sizeof(X[0]);
+   A_rot_fun(X,len); 
+   for(int x=0;x<len;x++) 
+            printf("%d ",*(X+x));
+            printf("\n");  
    return EXIT_SUCCESS ;
 }
