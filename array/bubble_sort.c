577e81dd786c886c04f5c87fe55aab9347109987
embedded.corelinux <embedded.corelinux@gmail.com>
2019-12-06 13:06:13 +0530

    This is demo of the bubble sort

---
+++ array/bubble_sort.c
@@ -0,0 +1,48 @@
+/** @subject: For the bubble short
+ * @author:Pramod Maurya
+ * @brief : This is demo explaine the bubble short algoritham 
+ * @version : 0.1
+ * @date : 2019-12-05
+ * @copyright Copyright (c) 2019
+ * @algoritham:[Assending Order]
+ *       Step :01 
+ *           a)Compaired two adjacent element in array like a[i] and a[i+1]
+ *           b)If you find condtion is true then swap these two adjasent array element
+ *               b.1)if(a[i] > a[i+1])
+ *               b.2)Swapped these array element and increment counter
+ *               b.3)if condition failed then mov to next element without swapping
+ *       Step  :02 
+ *            When swapping completed then return the swapped element in calling funtion. 
+ * */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int *swapped_c(int *A,int j)
+ {
+ 	    A[j]^=A[j+1];
+     	    A[j+1]^=A[j];
+            A[j]^=A[j+1];
+    	return A ;
+ }
+int* bubble_sort(int *A,int len)
+ {
+   	for(int i=0;i<len-1;i++)
+    	{
+      	    for(int j=0;j<len-i-1;j++)
+         	if(A[j]>A[j+1])
+              	  swapped_c(A,j) ; 
+       }
+        return A;
+ }  
+int main()
+   {
+ 	int  A[] = {40,20,50,60,30,10};
+ 	int len = sizeof(A)/sizeof(A[0]);
+ 	bubble_sort(A,len);
+ 		for(int i=0;i<len;i++)
+       		  printf("%d ",A[i]);
+   	printf("\n");
+ 	return EXIT_SUCCESS; 
+ } 
+
