2012f208927ff4002ea56f0917c3026fae913546
embedded.corelinux <embedded.corelinux@gmail.com>
2019-12-11 18:59:40 +0530

    This little bit optimization....!

--- array/array_rotation.c
+++ array/array_rotation.c
@@ -1,5 +1,5 @@
 /* About Code :
- *              This code is written for ratating desired element from left to write
+ *              This code is written for ratating desired element from left to write This is very short and easy
  * Author     : Pramod Kumar Maurya
  * Date       : 23/11/2019            
  **/
@@ -7,44 +7,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int* Temp_buffering(int *array,int shift_element,int *temp)
+int* Array_Roation(int *a,int len,int shift_element)
 {
-    int i=0;
-             while (i<shift_element)
-            {
-                   temp[i]=array[i];
-                   i++;
-             }
-    return temp;
-}
-
-int *Array_swapping(int *array,int len,int shift_element)
-{
-   for(int i=0;i<shift_element;i++)
-          {
-            for(int j=0;j<len;j++)
-                      array[j]=array[j+1];
-          }
-    return array;
-}
-
-int* Array_Roation(int *array,int len,int shift_element)
-{
-      int temp[shift_element];
-      Temp_buffering(array,shift_element,temp);  
-      Array_swapping(array,len,shift_element); 
-      for(int i=0;i<shift_element;i++)
-                      array[len-shift_element+i] = temp[i];        
-       return array;
+    int temp; 
+    while(shift_element--)
+         {
+                temp =a[0];                       //This produced wrong illusion it will store 0,1...d element
+                 for(int k=0; k<len; k++)
+                      a[k]=a[k+1];               
+                a[len-1]=temp;             //Here we are updating the array last element at end of rotation
+         }                   
+       return a;
 }
 
 int main()
 {
-   int X[] ={1,4,6,7,9,8,5},d=2;
+   int X[] ={1,4,6,7,9,8,5},d=7;
    int len = sizeof(X)/sizeof(X[0]);
    Array_Roation(X,len,d); 
       for(int x=0;x<len;x++) 
               printf("%d ",*(X+x));
-              printf("\n");  
-   return EXIT_SUCCESS ;
+              printf("\n");
+ 
+    return EXIT_SUCCESS ;
 }
